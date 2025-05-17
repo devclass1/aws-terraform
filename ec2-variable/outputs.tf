225577c0d4af538278641090882ef3bdf4fb3957
devclass1 <72622124+devclass1@users.noreply.github.com>
2025-05-17 22:19:13 +0530

    Create outputs.tf

---
+++ ec2-variable/outputs.tf
@@ -0,0 +1,29 @@
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
+
+output "subnet_id" {
+  description = "ID of the subnet"
+  value       = aws_subnet.main.id
+}
+
+output "ec2_instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.main.id
+}
+
+output "ec2_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.main.public_ip
+}
+
+output "ec2_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.main.public_dns
+}
+
+output "security_group_id" {
+  description = "ID of the security group"
+  value       = aws_security_group.allow_ssh_http.id
+}
